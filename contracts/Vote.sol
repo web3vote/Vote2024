abf12c7e06c0ba3a0abb65392fbe838ed4db71ff
JackBekket <jackatwork@yandex.ru>
2024-01-15 16:57:35 +0400

    create new vote

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -41,15 +41,16 @@ contract Vote is Ownable, AccessControl {
 
 
     struct Voting {
+        uint uid;
         Org _org ;
         string desc; // description
         uint start_date;
         //uint end_date;
         uint time_to_vote_hours;
         Passport.PassportType id_type_required; // can vote only internal or only external. by default if all citizens have internal passport, everyone is allowed. opposition in exile can be elected only by exiled russians if seted.
-        mapping(string option => uint count) free_results;  // if it's "open promt vote" than we just count how much for each option
+       // mapping(string option => uint count) free_results;  // if it's "open promt vote" than we just count how much for each option
         uint votes_total;
-        mapping(address => bool) users_voted;
+      //  mapping(address => bool) users_voted;
     }
 
     struct Org {
@@ -60,6 +61,10 @@ contract Vote is Ownable, AccessControl {
     mapping (uint uid_vote => Voting) public Votings; // All voting event by id
     mapping (address orginiser =>Voting) public VotingsByOrg;   // can be a few
 
+    //mapping (Voting voting => mapping(string option => uint count)) public Results;
+    mapping (uint uid_vote => mapping(string option => uint count)) public VoteResultsFreePromt;
+    mapping (uint uid_vote => mapping(address user => bool voted)) private UsersVoted;
+
     function createNewVote(address orginiser_or_ens, address operator, uint start_date_timestamp, uint vote_hours, Passport.PassportType id_type_required) onlyOwner() public{
         Voting storage v = Votings[uid_vote_global_counter];
         Org memory o = Org(orginiser_or_ens, operator);
@@ -68,6 +73,8 @@ contract Vote is Ownable, AccessControl {
         v.time_to_vote_hours = vote_hours;
         v.id_type_required = id_type_required;
         Votings[uid_vote_global_counter] = v;
+        VotingsByOrg[orginiser_or_ens] = v;
+        uid_vote_global_counter +=1;
     }
 
 }
